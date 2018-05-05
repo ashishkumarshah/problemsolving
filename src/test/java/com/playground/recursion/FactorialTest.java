ff8c8b0cbdde75739fdafe2f4ffda73dbb7d4603
Ashish Kumar Shah <ashishkumarshah@gmail.com>
2018-05-05 21:08:09 +0530

    Adding Test for negetive value passed for factorial

--- src/test/java/com/playground/recursion/FactorialTest.java
+++ src/test/java/com/playground/recursion/FactorialTest.java
@@ -7,11 +7,16 @@ import static org.junit.Assert.*;
 
 public class FactorialTest {
     @Test public void testFactorial() {
-	assertEquals(Factorial.factorial(2),2);
-	assertEquals(Factorial.factorial(1),1);
-	assertEquals(Factorial.factorial(0),1);
-	assertEquals(Factorial.factorial(4),24);
-	assertEquals(Factorial.factorial(5),120);
-	//assertEquals(Factorial.factorial(-1),120);
+      assertEquals(Factorial.factorial(2),2);
+      assertEquals(Factorial.factorial(1),1);
+      assertEquals(Factorial.factorial(0),1);
+      assertEquals(Factorial.factorial(4),24);
+      assertEquals(Factorial.factorial(5),120);
+      try {
+        long retval = Factorial.factorial(-1);
+        fail("Exception Not Thrown");
+      } catch (IllegalArgumentException ex) {
+        
+      }
     }
 }
